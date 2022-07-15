0bab5c4abd69de5d96c157fbe20857411fe51c9c
sardormirzakomilov <mirzakomilovs@gmail.com>
2022-07-15 20:59:27 +0500

    no end form

--- script.js
+++ script.js
@@ -26,21 +26,29 @@ namee(close_bg)
 let login = document.querySelector('.login a');
 let login_bg = document.querySelector('.login_bg');
 let logindiv = document.querySelector('.logindiv');
-let register_buttonA = document.querySelector('.title_bottom a');
+let register = document.querySelector('.register');
+let register_buttonA = document.querySelector('.login__');
+
 let body = document.querySelector('body');
 
 login.addEventListener('click' , function (e) {
-    e.preventDefault()
     logindiv.style.top = '50%'
-    login_bg.style.top = '0'
+    login_bg.style.display = 'block'
     body.style.overflow = 'hidden'
     body.style.paddingRight = '17px'
 })
 
 login_bg.addEventListener('click' , function (e) {
-    e.preventDefault();
-    login_bg.style.top = '-100%'
+     login_bg.style.display ='none'
     logindiv.style.top = '-100%'
     body.style.overflow = 'auto'
     body.style.paddingRight = '0px'
+    register.style.display = 'none'
+})
+
+register_buttonA.addEventListener('click' , function (e) {
+
+    e.preventDefault()
+    logindiv.style.display = 'none'
+    register.style.display = 'block'
 })
